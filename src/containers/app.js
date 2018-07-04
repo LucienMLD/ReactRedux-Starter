af0c8dfc12704cb67c8a26e64ece7540b9bedae6
lucien mollard <mollard.lucien@gmail.com>
2018-07-04 10:43:32 +0200

    Day two3

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import SearchBar from "../components/search-bar";
+import VideoList from "./video-list";
+import VideoDetail from "../components/video-detail";
+import axios from "axios";
 
-const App = function() {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+const API_END_POINT = "https://api.themoviedb.org/3/";
+const POPULAR_MOVIES_URL =
+  "discover/movie?language=fr&sort_by=popularity.desc&include_adult=false&append_to_response=images";
+const API_KEY = "api_key=fc46f1ca7acbf330c3350445a3c2d686";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { movieList: {}, currentMovie: {} };
+  }
+  componentWillMount() {
+    this.initMovies();
+  }
+  initMovies() {
+    axios.get(`${API_END_POINT}${POPULAR_MOVIES_URL}&${API_KEY}`).then(
+      function(response) {
+        this.setState({
+          movieList: response.data.results.slice(1, 6),
+          currentMovie: response.data.results[0]
+        });
+      }.bind(this)
+    );
+  }
+  render() {
+    const renderVideoList = () => {
+      if (this.state.movieList.length >= 5) {
+        <VideoList movieList={this.state.movieList} />;
+        console.log(this.state.movieList);
+      }
+    };
+    return (
+      <div>
+        <SearchBar />
+        {renderVideoList()}
+        <VideoDetail
+          title={this.state.currentMovie.title}
+          description={this.state.currentMovie.overview}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
